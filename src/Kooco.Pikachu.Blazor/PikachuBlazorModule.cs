4d997c322d5f17302411b29f1f6a3703a4dd808a
Kenneth Liao <kenseco@kooco.com.tw>
2023-06-15 14:28:18 +0800

    slove cryptographicExceptions

--- src/Kooco.Pikachu.Blazor/PikachuBlazorModule.cs
+++ src/Kooco.Pikachu.Blazor/PikachuBlazorModule.cs
@@ -41,6 +41,9 @@ using Volo.Abp.UI;
 using Volo.Abp.UI.Navigation;
 using Volo.Abp.UI.Navigation.Urls;
 using Volo.Abp.VirtualFileSystem;
+using Volo.Abp.OpenIddict;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Kooco.Pikachu.Blazor;
 
@@ -52,7 +55,7 @@ namespace Kooco.Pikachu.Blazor;
     typeof(AbpSwashbuckleModule),
     typeof(AbpAspNetCoreSerilogModule),
     typeof(AbpAccountWebOpenIddictModule),
-    typeof(AbpAspNetCoreComponentsServerLeptonXLiteThemeModule),
+    typeof(AbpAspNetCoreComponentsServerLeptonXLiteThemeModule),    
     typeof(AbpAspNetCoreMvcUiLeptonXLiteThemeModule),
     typeof(AbpIdentityBlazorServerModule),
     typeof(AbpTenantManagementBlazorServerModule),
@@ -82,7 +85,106 @@ public class PikachuBlazorModule : AbpModule
                 options.UseLocalServer();
                 options.UseAspNetCore();
             });
+
+            var hostingEnvironment = context.Services.GetHostingEnvironment();
+
+            if (hostingEnvironment.IsDevelopment()) return;
+
+            PreConfigure<AbpOpenIddictAspNetCoreOptions>(options =>
+            {
+                options.AddDevelopmentEncryptionAndSigningCertificate = false;
+            });
+
+            PreConfigure<OpenIddictServerBuilder>(builder =>
+            {
+                builder.AddEncryptionCertificate(GetEncryptionCertificate(hostingEnvironment,
+                    context.Services.GetConfiguration()));
+                builder.AddSigningCertificate(
+                    GetSigningCertificate(hostingEnvironment, context.Services.GetConfiguration()));
+            });
         });
+
+        var hostingEnvironment = context.Services.GetHostingEnvironment();
+
+        if (hostingEnvironment.IsDevelopment()) return;
+
+        PreConfigure<AbpOpenIddictAspNetCoreOptions>(options =>
+        {
+            options.AddDevelopmentEncryptionAndSigningCertificate = false;
+        });
+
+        PreConfigure<OpenIddictServerBuilder>(builder =>
+        {
+            builder.AddEncryptionCertificate(GetEncryptionCertificate(hostingEnvironment,
+                context.Services.GetConfiguration()));
+            builder.AddSigningCertificate(
+                GetSigningCertificate(hostingEnvironment, context.Services.GetConfiguration()));
+        });
+    }
+
+    private X509Certificate2 GetEncryptionCertificate(IWebHostEnvironment environment, IConfiguration config)
+    {
+        var fileName = "encryption-certificate.pfx";
+        var password = config["MyAppCertificate:X590:Password"];
+
+        var file = Path.Combine(environment.ContentRootPath, fileName);
+        if (File.Exists(file))
+        {
+            var created = File.GetCreationTime(file);
+            var days = (DateTime.Now - created).TotalDays;
+            if (days > 180)
+            {
+                File.Delete(file);
+            }
+            else
+            {
+                return new X509Certificate2(file, password, X509KeyStorageFlags.MachineKeySet);
+            }
+        }
+
+
+        using var algorithm = RSA.Create(keySizeInBits: 2048);
+        var subject = new X500DistinguishedName("CN=Fabrikam Encryption Certificate");
+        var request = new CertificateRequest(subject, algorithm,
+            HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+        request.CertificateExtensions.Add(new X509KeyUsageExtension(
+            X509KeyUsageFlags.KeyEncipherment, critical: true));
+        var certificate = request.CreateSelfSigned(DateTimeOffset.UtcNow,
+            DateTimeOffset.UtcNow.AddYears(2));
+        File.WriteAllBytes(file, certificate.Export(X509ContentType.Pfx, password));
+        return new X509Certificate2(file, password, X509KeyStorageFlags.MachineKeySet);
+    }
+
+    private X509Certificate2 GetSigningCertificate(IWebHostEnvironment environment, IConfiguration config)
+    {
+        var fileName = "signing-certificate.pfx";
+        var password = config["MyAppCertificate:X590:Password"];
+        var file = Path.Combine(environment.ContentRootPath, fileName);
+
+        if (File.Exists(file))
+        {
+            var created = File.GetCreationTime(file);
+            var days = (DateTime.Now - created).TotalDays;
+            if (days > 180)
+            {
+                File.Delete(file);
+            }
+            else
+            {
+                return new X509Certificate2(file, password, X509KeyStorageFlags.MachineKeySet);
+            }
+        }
+
+        using var algorithm = RSA.Create(keySizeInBits: 2048);
+        var subject = new X500DistinguishedName("CN=Fabrikam Signing Certificate");
+        var request = new CertificateRequest(subject, algorithm, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+        request.CertificateExtensions.Add(new X509KeyUsageExtension(X509KeyUsageFlags.DigitalSignature,
+            critical: true));
+
+        var certificate = request.CreateSelfSigned(DateTimeOffset.UtcNow, DateTimeOffset.UtcNow.AddYears(2));
+
+        File.WriteAllBytes(file, certificate.Export(X509ContentType.Pfx, password));
+        return new X509Certificate2(file, password, X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.MachineKeySet);
     }
 
     public override void ConfigureServices(ServiceConfigurationContext context)
