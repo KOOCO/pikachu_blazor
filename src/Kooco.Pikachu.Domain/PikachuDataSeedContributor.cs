1fb2378680ae25e2c5d82ec0c1104532cc2a3d7e
kenseco <kenseco@kooco.com.tw>
2023-07-23 08:59:49 +0800

    fix data nullable

---
+++ src/Kooco.Pikachu.Domain/PikachuDataSeedContributor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+using Kooco.Pikachu.Items;
+
+namespace Kooco.Pikachu
+{
+    internal class PikachuDataSeedContributor : IDataSeedContributor, ITransientDependency
+    {
+        private readonly IRepository<Item, Guid> _appItemRepository;
+
+        public PikachuDataSeedContributor(IRepository<Item, Guid> appItemRepository)
+        {
+            _appItemRepository = appItemRepository;
+        }
+
+        public async Task SeedAsync(DataSeedContext context)
+        {
+            if (await _appItemRepository.GetCountAsync() <= 0)
+            {
+                await _appItemRepository.InsertAsync(
+                    new Item
+                    {
+                        ItemName = "SunShine Umbrella",
+                        ItemDescription = "This is a simple description of demo item",
+                        SellingPrice = 10,
+                        SKU = "APCJ-Blue-001"
+
+                    },
+                autoSave: true);
+            }
+        }
+    }
+  
+}
