b2c66f6070a50b5031944d2d2a9acaaa42696282
Jawad Chughtai <jwaad04@gmail.com>
2023-10-15 21:19:27 +0500

    Update OrderController.cs

--- src/Kooco.Pikachu.HttpApi/Controllers/Orders/OrderController.cs
+++ src/Kooco.Pikachu.HttpApi/Controllers/Orders/OrderController.cs
@@ -1,8 +1,6 @@
 ﻿using Kooco.Pikachu.Orders;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json.Linq;
 using System;
 using System.Threading.Tasks;
 using Volo.Abp;
@@ -66,9 +64,7 @@ namespace Kooco.Pikachu.Controllers.Orders
 
         [HttpPost("callback")]
         [AllowAnonymous]
-        [Consumes("application/x-www-form-urlencoded")]
-        [Produces("text/html")]
-        public async Task<IActionResult> HandleCallbackAsync([FromForm]PaymentResult info)
+        public async Task<IActionResult> HandleCallbackAsync()
         {
             // Handle the callback data here
             // This is just a placeholder. Replace with your actual handling code.
