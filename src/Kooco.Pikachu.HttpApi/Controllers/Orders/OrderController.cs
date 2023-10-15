66e4ab7501e3e5de54d783e433095966ce3d5274
Jawad Chughtai <jwaad04@gmail.com>
2023-10-15 21:06:33 +0500

    update order controller

--- src/Kooco.Pikachu.HttpApi/Controllers/Orders/OrderController.cs
+++ src/Kooco.Pikachu.HttpApi/Controllers/Orders/OrderController.cs
@@ -1,6 +1,8 @@
 ﻿using Kooco.Pikachu.Orders;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Threading.Tasks;
 using Volo.Abp;
@@ -64,13 +66,15 @@ namespace Kooco.Pikachu.Controllers.Orders
 
         [HttpPost("callback")]
         [AllowAnonymous]
-        public async Task<IActionResult> HandleCallbackAsync(PaymentResult result)
+        [Consumes("application/x-www-form-urlencoded")]
+        [Produces("text/html")]
+        public async Task<IActionResult> HandleCallbackAsync([FromForm]PaymentResult info)
         {
             // Handle the callback data here
             // This is just a placeholder. Replace with your actual handling code.
             try
             {
-                await _ordersAppService.HandlePaymentAsync(result);
+                //await _ordersAppService.HandlePaymentAsync(result);
                 return Ok("1|OK");
             }
             catch
@@ -80,7 +84,7 @@ namespace Kooco.Pikachu.Controllers.Orders
         }
 
         [HttpPost("dummy")]
-        Task IOrderAppService.HandlePaymentAsync(PaymentResult result)
+        public Task HandlePaymentAsync(PaymentResult result)
         {
             throw new NotImplementedException();
         }
