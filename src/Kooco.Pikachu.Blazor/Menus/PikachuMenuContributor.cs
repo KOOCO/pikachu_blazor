af2d44580daae8fc04f6df83b044f5f1193fb29f
kenseco <kenseco@kooco.com.tw>
2023-10-19 01:52:45 +0800

    localization

--- src/Kooco.Pikachu.Blazor/Menus/PikachuMenuContributor.cs
+++ src/Kooco.Pikachu.Blazor/Menus/PikachuMenuContributor.cs
@@ -96,12 +96,6 @@ public class PikachuMenuContributor : IMenuContributor
             name: "FreebieManagement",
             icon: "fas fa-gifts",
             displayName: "贈品設定",
-            url: "/Freebie/FreebieManagement")
-            )
-             .AddItem(new ApplicationMenuItem(
-            name: "Freebies",
-            icon: "fas fa-chalkboard-teacher",
-            displayName: "團購頁面設定d",
             url: "/Freebie/FreebieList")
             )
 
