142b2780393522deeee389542ecaa5e9a56af766
Mujahid Ali Kayani <98872876+MujahidAliKiani@users.noreply.github.com>
2024-02-16 16:06:45 +0300

    Fix for 167

--- src/Kooco.Pikachu.Blazor/Pages/GroupBuyManagement/CreateGroupBuy.razor.cs
+++ src/Kooco.Pikachu.Blazor/Pages/GroupBuyManagement/CreateGroupBuy.razor.cs
@@ -434,9 +434,15 @@ namespace Kooco.Pikachu.Blazor.Pages.GroupBuyManagement
                     await _uiMessageService.Warn(L[PikachuDomainErrorCodes.GroupBuyNameCannotBeNull]);
                     return;
                 }
+                if (CreateGroupBuyDto.ShortCode.IsNullOrWhiteSpace())
+                {
+                    await _uiMessageService.Warn(L[PikachuDomainErrorCodes.GroupBuyShortCodeCannotBeNull]);
+                    return;
+                }
 
                 string shortCode = CreateGroupBuyDto.ShortCode;
                 string pattern = @"^[A-Za-z0-9]{4,12}$";
+
                 bool isPatternValid = Regex.IsMatch(shortCode, pattern);
 
                 if (!isPatternValid)
--- src/Kooco.Pikachu.Domain.Shared/PikachuDomainErrorCodes.cs
+++ src/Kooco.Pikachu.Domain.Shared/PikachuDomainErrorCodes.cs
@@ -6,6 +6,7 @@ public static class PikachuDomainErrorCodes
 
     public const string ItemNameCannotBeNull = "Pikachu:ItemNameCannotBeNull";
     public const string GroupBuyNameCannotBeNull = "Pikachu:GroupBuyNameCannotBeNull";
+    public const string GroupBuyShortCodeCannotBeNull = "Pikachu:GroupBuyShortCodeCannotBeNull";
     public const string ItemWithSameNameAlreadyExists = "Pikachu:ItemWithSameNameAlreadyExists";
     public const string FreebieWithSameNameAlreadyExists = "Pikachu:FreebieWithSameNameAlreadyExists";
     public const string ItemDetailsCannotBeEmpty = "Pikachu:ItemDetailsCannotBeEmpty";
