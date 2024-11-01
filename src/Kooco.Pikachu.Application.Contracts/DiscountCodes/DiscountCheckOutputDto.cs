dfcd11211c3d05b8d0527951c2530a86e7e0f8d5
Mujahid Ali Kayani <98872876+MujahidAliKiani@users.noreply.github.com>
2024-11-01 11:16:01 +0500

    Commit For PK-472

---
+++ src/Kooco.Pikachu.Application.Contracts/DiscountCodes/DiscountCheckOutputDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Kooco.Pikachu.DiscountCodes
+{
+    
+    public class DiscountCheckOutputDto
+    {
+        public string DiscountType { get; set; }
+        public int? DiscountAmount { get; set; }
+        public int? DiscountPercentage { get; set; }
+        public int? MinimumSpendAmount { get; set; }
+        public string[] ShippingMethods { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
--- src/Kooco.Pikachu.Application/DiscountCodes/DiscountCodeAppService.cs
+++ src/Kooco.Pikachu.Application/DiscountCodes/DiscountCodeAppService.cs
@@ -10,6 +10,8 @@ using Volo.Abp;
 using Kooco.Pikachu.Items;
 using Kooco.Pikachu.Items.Dtos;
 using Kooco.Pikachu.GroupBuys;
+using Volo.Abp.Domain.Repositories;
+using Kooco.Pikachu.EnumValues;
 
 namespace Kooco.Pikachu.DiscountCodes
 {
@@ -75,6 +77,65 @@ namespace Kooco.Pikachu.DiscountCodes
             discountCode.Status = !discountCode.Status;
             await discountCodeRepository.UpdateAsync(discountCode);
             return;
+        }
+
+        public async Task<DiscountCheckOutputDto> CheckDiscountCodeAsync(DiscountCheckInputDto input)
+        {
+            var discount = await discountCodeRepository
+                .FirstOrDefaultAsync(d => d.SpecifiedCode == input.Code);
+           
+          
+            if (discount == null)
+            {
+                return new DiscountCheckOutputDto { ErrorMessage = "InvalidCode" };
+            }
+            if (discount.EndDate < DateTime.UtcNow)
+            {
+                return new DiscountCheckOutputDto { ErrorMessage = "DiscountExpired" };
+            }
+            if (!discount.Status)
+            {
+                return new DiscountCheckOutputDto { ErrorMessage = "DiscountClosed" };
+            }
+            await discountCodeRepository.EnsureCollectionLoadedAsync(discount, x => x.DiscountSpecificGroupbuys);
+            if (discount.GroupbuysScope == "AllGroupbuys" ||
+                discount.DiscountSpecificGroupbuys.Any(g => g.GroupbuyId == input.GroupbuyId))
+            {
+                var output = new DiscountCheckOutputDto
+                {
+                    DiscountType = discount.DiscountMethod
+                };
+                if (discount.DiscountAmount.HasValue&& discount.DiscountAmount>0)
+                {
+                    output.DiscountAmount = discount.DiscountAmount;
+                }
+                if (discount.DiscountPercentage.HasValue && discount.DiscountPercentage > 0)
+                {
+                    output.DiscountPercentage = discount.DiscountPercentage;
+                }
+                switch (discount.DiscountMethod)
+                {
+                    case "DirectDiscount":
+                     
+                        break;
+
+                    case "RequireSpending":
+                        output.MinimumSpendAmount = discount.MinimumSpendAmount;
+                        break;
+
+                    case "ShippingDiscount":
+                        output.ShippingMethods = discount.ShippingDiscountScope == "AllMethods"
+                            ? new[] { "AllMethods" }
+                            : discount?.SpecificShippingMethods.Select(m => Enum.GetName(typeof(DeliveryMethod), m)).ToArray();
+                        break;
+                }
+
+                return output;
+            }
+            else
+            {
+                return new DiscountCheckOutputDto { ErrorMessage = "DiscountNotForThisGroupbuy" };
+            }
         }
     }
 }
