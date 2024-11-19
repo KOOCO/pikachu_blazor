04662fec74c8a5e45875bad577e6b3658dfc3d8a
Veer Mevada <136318459+veermevada2001@users.noreply.github.com>
2024-11-19 12:02:27 +0530

    PK-620

--- src/Kooco.Pikachu.Blazor/Pages/GroupBuyManagement/DeliveryTimeConts.cs
+++ src/Kooco.Pikachu.Blazor/Pages/GroupBuyManagement/DeliveryTimeConts.cs
@@ -1,15 +1,14 @@
 ﻿using System.Collections.Generic;
 
-namespace Kooco.Pikachu.Blazor.Pages.GroupBuyManagement
-{
-    public static class DeliveryTimeConts
-    {
-        public static List<string> SelfPickUp = new() { "Weekday9To13", "Weekday14To18", "Weekend9To13", "Weekend14To18", "UnableToSpecifyDuringPeakPeriods", "PleaseContactUs" };
-        public static List<string> BlackCat = new() { "Before13PM", "Between14To18PM", "Inapplicable" };
-        public static List<string> BlackCatFreeze = new() { "Before13PM", "Between14To18PM", "Inapplicable" };
-        public static List<string> BlackCatFrozen = new() { "Before13PM", "Between14To18PM", "Inapplicable" };
-        public static List<string> HomeDelivery = new() { "Weekday9To13", "Weekday14To18", "Weekend9To13", "Weekend14To18", "UnableToSpecifyDuringPeakPeriods", "Inapplicable" };
-        public static List<string> DeliveredByStore = new() { "Weekday9To13", "Weekday14To18", "Weekend9To13", "Weekend14To18", "UnableToSpecifyDuringPeakPeriods", "Inapplicable" };
+namespace Kooco.Pikachu.Blazor.Pages.GroupBuyManagement;
 
-    }
+public static class DeliveryTimeConts
+{
+    public static List<string> SelfPickUp = new() { "Weekday9To13", "Weekday14To18", "Weekend9To13", "Weekend14To18", "UnableToSpecifyDuringPeakPeriods", "PleaseContactUs" };
+    public static List<string> BlackCat = new() { "Before13PM", "Between14To18PM", "Inapplicable" };
+    public static List<string> BlackCatFreeze = new() { "Before13PM", "Between14To18PM", "Inapplicable" };
+    public static List<string> BlackCatFrozen = new() { "Before13PM", "Between14To18PM", "Inapplicable" };
+    public static List<string> HomeDelivery = new() { "Weekday9To13", "Weekday14To18", "Weekend9To13", "Weekend14To18", "UnableToSpecifyDuringPeakPeriods", "Inapplicable" };
+    public static List<string> DeliveredByStore = new() { "Before13PM", "Between14To18PM", "Inapplicable" };
+
 }
