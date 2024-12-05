ffeec783a249909b3bdaf464c9b00a8af8da0ab9
Mujahid Ali Kayani <98872876+MujahidAliKiani@users.noreply.github.com>
2024-12-05 10:58:19 +0500

    Commit For PK-737

---
+++ src/Kooco.Pikachu.EntityFrameworkCore/Migrations/20241205052452_Add Cost Column in ItemDatail table.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kooco.Pikachu.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCostColumninItemDatailtable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "Cost",
+                table: "AppItemDetails",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cost",
+                table: "AppItemDetails");
+        }
+    }
+}
