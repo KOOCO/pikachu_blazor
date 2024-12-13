91c01bde7dce6bdc9a26ee14c4aab523124753ce
Mujahid Ali Kayani <98872876+MujahidAliKiani@users.noreply.github.com>
2024-12-13 09:31:00 +0500

    Commit for PK-780

--- src/Kooco.Pikachu.EntityFrameworkCore/Orders/EfCoreOrderRepository.cs
+++ src/Kooco.Pikachu.EntityFrameworkCore/Orders/EfCoreOrderRepository.cs
@@ -310,7 +310,7 @@ public class EfCoreOrderRepository : EfCoreRepository<PikachuDbContext, Order, G
             .WhereIf(startDate.HasValue, x => x.CreationTime.Date >= startDate.Value.Date)
             .WhereIf(endDate.HasValue, x => x.CreationTime.Date <= endDate.Value.Date)
             //.Where(x => x.ShippingStatus == ShippingStatus.Shipped)
-            .Where(x=>x.IsVoidInvoice==false && x.InvoiceStatus==InvoiceStatus.Issued);
+            .Where(x=>x.IsVoidInvoice==false && x.IssueStatus!=null);
             //.Where(x => x.OrderType != OrderType.MargeToNew);
     }
     private static IQueryable<Order> ApplyVoidFilters(IQueryable<Order> queryable,
