8148cbcbbc097168d519834f09a898b4473c80f1
Mujahid Ali Kayani <98872876+MujahidAliKiani@users.noreply.github.com>
2025-01-14 09:27:36 +0500

    Fix for Pk-877

--- src/Kooco.Pikachu.Blazor/Pages/Orders/Order.razor.cs
+++ src/Kooco.Pikachu.Blazor/Pages/Orders/Order.razor.cs
@@ -1286,34 +1286,35 @@ public partial class Order
         }
     }
 
-    public void OnCheckboxValueChanged(bool e, OrderDto order)
+    public async void OnCheckboxValueChanged(bool e, OrderDto order)
     {
         order.IsSelected = e;
 
         if (SelectedTabName is not "ToBeShipped") return;
-        var orderDeliveries = GetOrderDeliveries(order.OrderId);
+        var orderDeliveries = await _OrderDeliveryAppService.GetListByOrderAsync(order.OrderId);
         if (order.IsSelected)
         {
             OrdersSelected.Add(order);
 
-            NormalCount = orderDeliveries.Any(a => a.Items.Any(ai => ai.DeliveryTemperature is ItemStorageTemperature.Normal)) ? NormalCount + 1 : NormalCount;
+            NormalCount = orderDeliveries.Any(x=>x.Items.Any(ai => ai.DeliveryTemperature is ItemStorageTemperature.Normal)) ? NormalCount + 1 : NormalCount;
 
-            FreezeCount = orderDeliveries.Any(a => a.Items.Any(ai => ai.DeliveryTemperature is ItemStorageTemperature.Freeze)) ? FreezeCount + 1 : FreezeCount ;
+            FreezeCount = orderDeliveries.Any(x => x.Items.Any(ai => ai.DeliveryTemperature is ItemStorageTemperature.Freeze)) ? FreezeCount + 1 : FreezeCount ;
 
-            FrozenCount = orderDeliveries.Any(a => a.Items.Any(ai => ai.DeliveryTemperature is ItemStorageTemperature.Frozen)) ? FrozenCount + 1 : FrozenCount;
+            FrozenCount = orderDeliveries.Any(x => x.Items.Any(ai => ai.DeliveryTemperature is ItemStorageTemperature.Frozen)) ? FrozenCount + 1 : FrozenCount;
         }
         else
 
         {
             OrdersSelected.Remove(order);
-            NormalCount = orderDeliveries.Any(a => a.Items.Any(ai => ai.DeliveryTemperature is ItemStorageTemperature.Normal)) ?  NormalCount - 1 : NormalCount;
-            FreezeCount = orderDeliveries.Any(a => a.Items.Any(ai => ai.DeliveryTemperature is ItemStorageTemperature.Freeze))?FreezeCount - 1 : FreezeCount;
-           FrozenCount= orderDeliveries.Any(a => a.Items.Any(ai => ai.DeliveryTemperature is ItemStorageTemperature.Frozen)) ?  FrozenCount - 1 : FrozenCount;
+            NormalCount = orderDeliveries.Any(x => x.Items.Any(ai => ai.DeliveryTemperature is ItemStorageTemperature.Normal)) ?  NormalCount - 1 : NormalCount;
+            FreezeCount = orderDeliveries.Any(x => x.Items.Any(ai => ai.DeliveryTemperature is ItemStorageTemperature.Freeze))?FreezeCount - 1 : FreezeCount;
+           FrozenCount= orderDeliveries.Any(x => x.Items.Any(ai => ai.DeliveryTemperature is ItemStorageTemperature.Frozen)) ?  FrozenCount - 1 : FrozenCount;
 
 
         }
-        
-     
+        StateHasChanged();
+
+
     }
 
     async Task OnSearch(Guid? e = null)
