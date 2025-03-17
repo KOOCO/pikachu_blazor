48dc976c7467bdd6ee8c4c0b6cb2a3fa25e79b19
Jawad Chughtai <jwaad04@gmail.com>
2025-03-17 07:37:29 +0500

    #PK-1164 All fields are required

--- src/Kooco.Pikachu.Application.Contracts/ElectronicInvoiceSettings/CreateUpdateElectronicInvoiceDto.cs
+++ src/Kooco.Pikachu.Application.Contracts/ElectronicInvoiceSettings/CreateUpdateElectronicInvoiceDto.cs
@@ -9,18 +9,23 @@ namespace Kooco.Pikachu.ElectronicInvoiceSettings
     public class CreateUpdateElectronicInvoiceDto
     {
         public bool IsEnable { get; set; }
+        
         [Required]
         public string StoreCode { get; set; }
+        
         [Required]
         public string HashKey { get; set; }
+        
         [Required]
         public string HashIV { get; set; }
+        
         [Required]
         public string DisplayInvoiceName { get; set; }
-        [Required]
         
-        public int DaysAfterShipmentGenerateInvoice { get; set; }
         [Required]
-        public DeliveryStatus StatusOnInvoiceIssue { get; set; }
+        public int? DaysAfterShipmentGenerateInvoice { get; set; }
+        
+        [Required]
+        public DeliveryStatus? StatusOnInvoiceIssue { get; set; }
     }
 }
