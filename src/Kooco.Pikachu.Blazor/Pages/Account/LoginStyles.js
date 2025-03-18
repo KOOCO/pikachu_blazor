16086b6ef656451cc2c90cbc6d93563a295457ec
Jawad Chughtai <jwaad04@gmail.com>
2025-03-18 08:15:15 +0500

    #PK-1174 Redesign login page

---
+++ src/Kooco.Pikachu.Blazor/Pages/Account/LoginStyles.js
@@ -0,0 +1,21 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const container = document.createElement("div");
+    container.classList.add("background-elements");
+
+    container.innerHTML = `
+        <div class="background-gradient"></div>
+        <div class="blob blob-1"></div>
+        <div class="blob blob-2"></div>
+        <div class="blob blob-3"></div>
+        <div class="blob blob-4"></div>
+        <div class="blob blob-5"></div>
+        <div class="float-element float-1"></div>
+        <div class="float-element float-2"></div>
+        <div class="float-element float-3"></div>
+        <div class="pulse-light pulse-1"></div>
+        <div class="pulse-light pulse-2"></div>
+        ${Array(7).fill('<div class="bubble"></div>').join('')}
+    `;
+
+    document.body.appendChild(container);
+});
