ac84a02dbf6c6bcacf447ac52034c513ce3bfadd
Eli Hu <elihu.develop@gmail.com>
2025-04-01 12:42:49 +0800

    feat: 🎸 Add_Table_TenantWallets_and_Transaction

---
+++ src/Kooco.Pikachu.EntityFrameworkCore/Migrations/20250401043907_Add_Table_AppTenantWallets_and_AppTenantWalletTransactions.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kooco.Pikachu.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Table_AppTenantWallets_and_AppTenantWalletTransactions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppTenantWallets",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TenantId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    WalletBalance = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ExtraProperties = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatorId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    LastModificationTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModifierId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false, defaultValue: false),
+                    DeleterId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DeletionTime = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppTenantWallets", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppTenantWalletTransactions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DeductionStatus = table.Column<int>(type: "int", nullable: false),
+                    TradingMethods = table.Column<int>(type: "int", nullable: false),
+                    TransactionType = table.Column<int>(type: "int", nullable: false),
+                    TransactionAmount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    TransactionNotes = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    TenantWalletId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatorId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    LastModificationTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModifierId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false, defaultValue: false),
+                    DeleterId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DeletionTime = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppTenantWalletTransactions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AppTenantWalletTransactions_AppTenantWallets_TenantWalletId",
+                        column: x => x.TenantWalletId,
+                        principalTable: "AppTenantWallets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppTenantWallets_TenantId",
+                table: "AppTenantWallets",
+                column: "TenantId",
+                unique: true,
+                filter: "[TenantId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppTenantWalletTransactions_TenantWalletId",
+                table: "AppTenantWalletTransactions",
+                column: "TenantWalletId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppTenantWalletTransactions");
+
+            migrationBuilder.DropTable(
+                name: "AppTenantWallets");
+        }
+    }
+}
