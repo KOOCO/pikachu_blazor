e0c1485dc6b4b7f752a05c6e783ef02e25e57f13
Kenseco <kenseco@kooco.com.tw>
2025-04-07 18:04:56 +0800

    Update OrderController.cs

--- src/Kooco.Pikachu.HttpApi/Controllers/Orders/OrderController.cs
+++ src/Kooco.Pikachu.HttpApi/Controllers/Orders/OrderController.cs
@@ -643,6 +643,7 @@ public class OrderController : AbpController, IOrderAppService
         return _ordersAppService.GetReturnOrderNotificationCount();
     }
     [HttpGet("get-order-id")]
+    [AllowAnonymous]
     public Task<Guid> GetOrderIdAsync(string orderNo)
     {
         return _ordersAppService.GetOrderIdAsync(orderNo);
