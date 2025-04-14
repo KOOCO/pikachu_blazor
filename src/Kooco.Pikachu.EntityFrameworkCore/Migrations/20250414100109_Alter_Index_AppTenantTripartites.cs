6e96f9d8cea74f903881224c774b0427a146ccfb
Eli Hu <elihu.develop@gmail.com>
2025-04-14 18:03:47 +0800

    feat: 🎸 Alter_Index_AppTenantTripartites

---
+++ src/Kooco.Pikachu.EntityFrameworkCore/Migrations/20250414100109_Alter_Index_AppTenantTripartites.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kooco.Pikachu.Migrations
+{
+    /// <inheritdoc />
+    public partial class Alter_Index_AppTenantTripartites : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_AppTenantTripartites_TenantId",
+                table: "AppTenantTripartites",
+                column: "TenantId",
+                unique: true,
+                filter: "[TenantId] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AppTenantTripartites_TenantId",
+                table: "AppTenantTripartites");
+        }
+    }
+}
