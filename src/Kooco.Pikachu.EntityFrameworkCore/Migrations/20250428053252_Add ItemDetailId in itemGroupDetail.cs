1220c7b9f753b8b7c38bf0725ad527cf2ed87711
Mujahid Ali Kayani <98872876+MujahidAliKiani@users.noreply.github.com>
2025-04-28 10:35:26 +0500

    fix

---
+++ src/Kooco.Pikachu.EntityFrameworkCore/Migrations/20250428053252_Add ItemDetailId in itemGroupDetail.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kooco.Pikachu.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddItemDetailIdinitemGroupDetail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ApplicableItems",
+                table: "AppShoppingCreditUsageSettings",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ItemDetailId",
+                table: "AppGroupBuyItemGroupDetails",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ItemDetailId",
+                table: "AppGroupBuyItemGroupDetails");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ApplicableItems",
+                table: "AppShoppingCreditUsageSettings",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
