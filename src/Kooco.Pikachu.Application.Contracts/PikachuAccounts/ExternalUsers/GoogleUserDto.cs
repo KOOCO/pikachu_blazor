0a2b32c741ef874487fe730d807d361abd5f85f2
Mujahid Ali Kayani <98872876+MujahidAliKiani@users.noreply.github.com>
2025-06-11 11:53:34 +0500

    Commit For Google Login issue

--- src/Kooco.Pikachu.Application.Contracts/PikachuAccounts/ExternalUsers/GoogleUserDto.cs
+++ src/Kooco.Pikachu.Application.Contracts/PikachuAccounts/ExternalUsers/GoogleUserDto.cs
@@ -1,11 +1,24 @@
-﻿namespace Kooco.Pikachu.PikachuAccounts.ExternalUsers;
+﻿using System.Text.Json.Serialization;
+
+namespace Kooco.Pikachu.PikachuAccounts.ExternalUsers;
 
 public class GoogleUserDto
 {
-    public string Sub { get; set; }  // Google's unique user ID
+    [JsonPropertyName("sub")]
+    public string Sub { get; set; }
+
+    [JsonPropertyName("name")]
     public string Name { get; set; }
+
+    [JsonPropertyName("given_name")]
     public string GivenName { get; set; }
+
+    [JsonPropertyName("family_name")]
     public string FamilyName { get; set; }
+
+    [JsonPropertyName("email")]
     public string Email { get; set; }
+
+    [JsonPropertyName("picture")]
     public string Picture { get; set; }
 }
