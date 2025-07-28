f500b4e23529a873444db05031c1803f5896f162
Mujahid Ali Kayani <98872876+MujahidAliKiani@users.noreply.github.com>
2025-07-28 09:20:47 +0500

    Commit For Pk-1721

--- src/Kooco.Pikachu.EntityFrameworkCore/Repositories/Orders/OrderRepository.cs
+++ src/Kooco.Pikachu.EntityFrameworkCore/Repositories/Orders/OrderRepository.cs
@@ -590,7 +590,7 @@ public class OrderRepository(IDbContextProvider<PikachuDbContext> dbContextProvi
     }
     public async Task<long> ReturnOrderNotificationCountAsync()
     {
-        return await ApplyReturnFilters((await GetQueryableAsync()).Include(o => o.GroupBuy), null, null, null).Where(x => (x.OrderStatus == OrderStatus.Returned || x.OrderStatus == OrderStatus.Exchange) && x.ReturnStatus == OrderReturnStatus.Pending).CountAsync();
+        return await ApplyReturnFilters((await GetQueryableAsync()).Include(o => o.GroupBuy), null, null, null).Where(x => (x.OrderStatus == OrderStatus.Returned || x.OrderStatus == OrderStatus.Exchange) && (x.ReturnStatus == OrderReturnStatus.Pending||x.ReturnStatus==OrderReturnStatus.Processing)).CountAsync();
     }
 
     public async Task<List<Order>> GetReturnListAsync(int skipCount, int maxResultCount, string? sorting, string? filter, Guid? groupBuyId)
